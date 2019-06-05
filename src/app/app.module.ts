cdfc814f5e6b6def6c5082b319d6da79e7b25395
pschmoek <philip.schmoekel@googlemail.com>
2019-06-05 14:37:57 +0200

    implement rxjs exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './core/containers/app/app.component';
+import { TodoModule } from './todo/todo.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
