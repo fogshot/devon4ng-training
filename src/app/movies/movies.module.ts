9075b41ed3f69a2d3ead20e5928bfc21d8a63490
Unknown <fogshot@gmail.com>
2019-07-12 10:52:50 +0200

    create movie overview component

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieOverviewComponent } from './movie-overview/movie-overview.component';
 
 @NgModule({
-  declarations: [MovieDetailsComponent],
+  declarations: [MovieDetailsComponent, MovieOverviewComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    MovieDetailsComponent
+    MovieOverviewComponent
   ]
 })
 export class MoviesModule { }
