8387370a0f00f3c86a3bffd11fd0984cbb7d7b6c
Unknown <fogshot@gmail.com>
2019-07-12 12:42:26 +0200

    complete exercise 2

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Movie} from '../movie';
 
 @Component({
@@ -6,19 +6,7 @@ import {Movie} from '../movie';
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent implements OnInit {
-  currentMovie: Movie;
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.currentMovie = {
-      id: 1, title: 'Pulp Fiction',
-      description: 'Pulp Fiction is a 1994 American crime film that tells several stories of criminal Los Angeles.',
-      directors: 'Quentin Tarantino',
-      year: 1994
-    };
-  }
+export class MovieDetailsComponent {
+  @Input() movie: Movie;
 
 }
--- src/app/movies/movie-overview/movie-overview.component.ts
+++ src/app/movies/movie-overview/movie-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../movie";
 
 @Component({
   selector: 'app-movie-overview',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieOverviewComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[] = [];
+  selectedMovie: Movie;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.movies.push({
+      id: 1, title: 'Pulp Fiction',
+      description: 'Pulp Fiction is a 1994 American crime film that tells several stories of criminal Los Angeles.',
+      directors: 'Quentin Tarantino',
+      year: 1994
+    }, {
+      id: 2,
+      title: 'The Shawshank Redemption',
+      description: 'An American film drama that tells the story of banker Andy Dufresne who ' +
+        'is sentenced to life for the murder of his wife and her lover, despite his claims of innocence.',
+      directors: 'Frank Darabont',
+      year: 1994
+    }, {
+      id: 3,
+      title: 'The Godfather',
+      description: 'An American crime film that chronicles the fictional New York crime family Corleone under the patriarch Vito Corleone.',
+      directors: 'Francis Ford Coppola',
+      year: 1972
+    });
+  }
+
+  selectMovie(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
+
+  isMovieSelected(movie: Movie): boolean {
+    return movie === this.selectedMovie;
+  }
 }
